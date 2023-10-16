d66fbd5c2016112f63ea9f5225c9878d8589172d
JackWu <625769020@qq.com>
2023-10-16 17:45:27 +0800

    Able to loop

--- tty-wr.c
+++ tty-wr.c
@@ -19,6 +19,12 @@ static void help(int argc, char **argv)
     char *app = basename(argv[0]);
     fprintf(stderr, "Usage:\n");
     fprintf(stderr, "    %s <device> <baudrate> <byte1> <byte2> [...]\n\n", app);
+
+    fprintf(stderr, "Env:\n");
+    fprintf(stderr, "    PROT=aimy, protocal\n");  
+    fprintf(stderr, "    LOOP=100, loop count\n");  
+    fprintf(stderr, "    LOOP_DELAY=100, loop delay, in ms\n\n");
+
     fprintf(stderr, "Exmaple:\n");
     fprintf(stderr, "    %s /dev/tty_mcu 115200                                         # monitor mode\n", app);
     fprintf(stderr, "    %s /dev/tty_mcu 115200 0b 55 aa 00 06 21 06 00 01 ff ff 20\n", app);
@@ -26,13 +32,16 @@ static void help(int argc, char **argv)
     fprintf(stderr, "    %s /dev/tty_58g 57600 55 aa 01 51 00 00                        # 58g read rf status\n", app);
     fprintf(stderr, "    %s /dev/tty_bp1048 460800 06 55 AA 80 01 A1 20\n", app);  
     fprintf(stderr, "    PROT=aimy %s /dev/tty_bp1048 460800 06 55 AA 80 01 A1 20\n", app);
+    fprintf(stderr, "    LOOP=100 LOOP_DELAY=1000 PROT=aimy %s /dev/tty_bp1048 460800 06 55 AA 80 01 A1 20   # test 100 tims\n", app);
 }
 
 static char lockfile[128];
 static void lockfile_remove(void)
 {
-    if (lockfile[0])
+    if (lockfile[0]) {
         unlink(lockfile);
+        sync();
+    }
 }
 
 static int lockfile_create(void)
@@ -64,7 +73,7 @@ static int lockfile_create(void)
 static void handle_signal(int sig)
 {
     (void)sig;
-    //printf("cleanup...\n");
+    printf("cleanup...\n");
     lockfile_remove();
     exit(1);
 }
@@ -81,6 +90,21 @@ int main(int argc, char **argv)
     bool first_byte = true;
     bool fist_byte_is_len = false;
     char *prot=getenv("PROT");
+    char *loop_env=getenv("LOOP");
+    int loop = 1;
+    
+    char *loop_delay_env=getenv("LOOP_DELAY");
+    int loop_delay = 0;                                         // ms
+    
+    if (loop_env)
+        loop = strtoul(loop_env, NULL, 10);
+    if (loop < 1)
+        loop = 1;
+
+    if (loop_delay_env)
+        loop_delay = strtoul(loop_delay_env, NULL, 10) * 1000;
+    if (loop_delay < 0)
+        loop_delay = 0;
 
     if (argc < 3) {
         help(argc, argv);
@@ -116,45 +140,49 @@ int main(int argc, char **argv)
         goto err2;
     }
 
-    if (len > 0) {
-        if (serial_write(serial, tx_data, len) < 0) {
-            fprintf(stderr, "serial_write(): %s\n", serial_errmsg(serial));
-            goto err1;
-        }        
-    }
-
-    if (prot != NULL && !strcmp(prot, "aimy")) {
-        fist_byte_is_len = true;
-    }
-
-    int rx_total=-1, rx_len=0;
-    while (1) {
-        if (len == 0 ) {
-            wait_timeout_ms = -1;
-        } else {
-            wait_timeout_ms = 3000;
+    while(loop--) {
+        usleep(loop_delay);
+        if (len > 0) {
+            if (serial_write(serial, tx_data, len) < 0) {
+                fprintf(stderr, "serial_write(): %s\n", serial_errmsg(serial));
+                goto err1;
+            }        
         }
 
-        if ((ret = serial_read(serial, rx_data, 1, wait_timeout_ms)) < 0) {
-            fprintf(stderr, "serial_read(): %s\n", serial_errmsg(serial));
-            goto err1;
+        if (prot != NULL && !strcmp(prot, "aimy")) {
+            fist_byte_is_len = true;
         }
-        if (ret > 0) {
-            if (first_byte == true && fist_byte_is_len == true) {
-                rx_total = rx_data[0]+1;
-                first_byte = false;
+
+        int rx_total=-1, rx_len=0;
+        first_byte = true;
+        while (1) {
+            if (len == 0 ) {
+                wait_timeout_ms = -1;
+            } else {
+                wait_timeout_ms = 3000;
             }
-            for (int i = 0; i < ret; i++) {
-                printf("%02x ", rx_data[i]);
-                rx_len++;
+
+            if ((ret = serial_read(serial, rx_data, 1, wait_timeout_ms)) < 0) {
+                fprintf(stderr, "serial_read(): %s\n", serial_errmsg(serial));
+                goto err1;
             }
-            if (rx_total != -1 && rx_len >= rx_total)
+            if (ret > 0) {
+                if (first_byte == true && fist_byte_is_len == true) {
+                    rx_total = rx_data[0]+1;
+                    first_byte = false;
+                }
+                for (int i = 0; i < ret; i++) {
+                    printf("%02x ", rx_data[i]);
+                    rx_len++;
+                }
+                if (rx_total != -1 && rx_len >= rx_total)
+                    break;
+            } else {
                 break;
-        } else {
-            break;
+            }
         }
+        printf("\n");
     }
-    printf("\n");
 
 err1:
     serial_close(serial);
